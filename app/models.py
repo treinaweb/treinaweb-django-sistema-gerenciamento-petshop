4ac03414814c36e151e71d59452907e1eae6167b
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-06-30 10:57:40 -0300

    02.01_Criando_Model_Cliente

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_localflavor_br.br_states import STATE_CHOICES
 
 # Create your models here.
+
+class EnderecoCliente(models.Model):
+    rua = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=30, null=False, blank=False)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES, null=False, blank=False)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    endereco = models.ForeignKey(EnderecoCliente, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=14, null=False, blank=False)
+    data_nascimento = models.DateField(null=False, blank=False)
+    profissao = models.CharField(max_length=25, null=False, blank=False)
